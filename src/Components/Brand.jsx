fc07bffa987651d206412d8be716161380f9b4bf
plainspoken-kiran <kiran@plainspoken.com>
2023-11-21 12:23:39 -0500

    cleaned

---
+++ src/Components/Brand.jsx
@@ -0,0 +1,48 @@
+import React, { Fragment, useEffect } from "react";
+import { StyleSheet, css } from "aphrodite/no-important";
+import { useParams, useRouteMatch, Switch, Route } from "react-router-dom";
+
+import Model from "./Model";
+import SubMenu from "./SubMenu";
+
+const styles = StyleSheet.create({
+	conatiner: {
+		display: "flex",
+	},
+	row: {
+		padding: "5px",
+	},
+	h1: {
+		fontWeight: "300",
+		margin: "10px 20px",
+		fontSize: "45px",
+		color: "#a1352a",
+	},
+});
+
+const Brand = () => {
+	const { brand } = useParams();
+	const { url } = useRouteMatch();
+
+	useEffect(() => {
+		console.log(JSON.stringify(url));
+	},[url]);
+
+	return (
+		<Fragment>
+			<h1 className={css(styles.h1)}>{brand}</h1>
+			<div className={css(styles.conatiner)}>
+				<SubMenu />
+				<div className={css(styles.row)}>
+					<Switch>
+						<Route path={`${url}/:model`}>
+							<Model />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Brand;
